179f564618e1b74ef52432c4acb33bfb3370e736
EugeneMedov <medovphotoart@gmail.com>
2023-12-16 17:54:24 +0300

    прикрутил бутстрап

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <title>Сократитель ссылок</title>
 </head>
 <body>
@@ -10,7 +11,7 @@
 <form id="shortenForm">
     <label for="originalUrl">Оригинальная ссылка:</label>
     <input type="text" name="originalUrl" id="originalUrl" required>
-    <button type="button" onclick="shortenUrl()">Сократить</button>
+    <button type="button" class="btn btn-primary" onclick="shortenUrl()">Сократить</button>
 </form>
 
 <br>
